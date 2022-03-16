9d059d88d0959fb4c6e02c83a57ad3ab469e32c8
thisiskeanyvy <notifkeanyvy@gmail.com>
2022-03-16 05:57:57 +0100

    aliexpress product id

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,11 @@ function saveOrders() {
       //Amazon
       if(domain.includes("www.amazon") == true) {
         var product_url = tabs[0].url;
-        if (product_url.includes("?") && product_url.includes("dp")) {
+        if(product_url.includes("?") == true && product_url.includes("dp") == true) {
           var product_id = product_url.split("dp")[1].replace("/","").replace("/","");
-          if (product_id.includes("?")) {
+          if(product_id.includes("?")) {
             product_id = product_id.split("?")[0];
-            if(product_id.includes("ref=")) {
+            if(product_id.includes("ref=") == true) {
               product_id = product_id.split("ref=")[0]
             }
             alert(product_id)
@@ -20,12 +20,21 @@ function saveOrders() {
         } else {
           alert("Product not found...")
         }
-      } else {
-        alert("Domain not found...")
+      }
+      //AliExpress
+      if(domain.includes("aliexpress.com") == true) {
+        var product_url = tabs[0].url;
+        if(product_url.includes(".html") == true && product_url.includes("/item/") == true) {
+          var product_id = product_url.split("item")[1].split(".html")[0].replace("/","");
+          alert(product_id)
+        } else {
+          alert("Product not found...")
+        }
       }
   });
   //save to localstorage with JSON structure
   //Amazon short url : https://www.amazon.fr/dp/B07PHPXHQS/
+  //AliExpress short url : https://fr.aliexpress.com/item/1005003463410974.html
 }
 
 function getBalance() {

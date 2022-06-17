2672b2dce05c661873f0c029f2120eff15515992
thisiskeanyvy <notifkeanyvy@gmail.com>
2022-06-17 12:53:32 +0200

    update manifest v2 to manifest v3

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,41 @@ function saveOrders() {
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
       var domain = new URL(tabs[0].url).hostname;
       //Amazon
-      if(domain.includes("www.amazon") == true) {
-        var product_url = tabs[0].url;
-        if(product_url.includes("?") == true && product_url.includes("dp") == true) {
-          var product_id = product_url.split("dp")[1].replace("/","").replace("/","");
-          if(product_id.includes("?")) {
-            product_id = product_id.split("?")[0];
-            if(product_id.includes("ref=") == true) {
-              product_id = product_id.split("ref=")[0]
+      try {
+        if(domain.includes("www.amazon") == false || domain.includes("aliexpress.com") == false ) {
+          document.getElementById("statusHostname").innerHTML = "Marketplace not recognized."
+        }
+        if(domain.includes("www.amazon") == true) {
+          document.getElementById("statusHostname").innerHTML = "Amazon.com"
+          var product_url = tabs[0].url;
+          if(product_url.includes("?") == true && product_url.includes("dp") == true) {
+            var product_id = product_url.split("dp")[1].replace("/","").replace("/","");
+            if(product_id.includes("?")) {
+              product_id = product_id.split("?")[0];
+              if(product_id.includes("ref=") == true) {
+                product_id = product_id.split("ref=")[0]
+              }
+              alert(product_id)
             }
-            alert(product_id)
+          } else {
+            document.getElementById("statusHostname").innerHTML = "Amazon.com"
+            alert("Product not found...")
           }
-        } else {
-          alert("Product not found...")
         }
-      }
-      //AliExpress
-      if(domain.includes("aliexpress.com") == true) {
-        var product_url = tabs[0].url;
-        if(product_url.includes(".html") == true && product_url.includes("/item/") == true) {
-          var product_id = product_url.split("item")[1].split(".html")[0].replace("/","");
-          alert(product_id)
-        } else {
-          alert("Product not found...")
+        //AliExpress
+        if(domain.includes("aliexpress.com") == true) {
+          document.getElementById("statusHostname").innerHTML = "AliExpress.com"
+          var product_url = tabs[0].url;
+          if(product_url.includes(".html") == true && product_url.includes("/item/") == true) {
+            var product_id = product_url.split("item")[1].split(".html")[0].replace("/","");
+            alert(product_id)
+          } else {
+            document.getElementById("statusHostname").innerHTML = "AliExpress.com"
+            alert("Product not found...")
+          }
         }
+      } catch {
+        document.getElementById("statusHostname").innerHTML += "An error has occurred."
       }
   });
   //save to localstorage with JSON structure
